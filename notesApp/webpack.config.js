286cc59acc7ac93d3815022336edd38d63b89b34
AleksanderIII <alex-llewton@mail.ru>
2018-08-27 12:44:35 +0300

    fix webpack.config

--- notesApp/webpack.config.js
+++ notesApp/webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
         publicPath: "build/",
         filename: "bundle.js"
     },
+    moduleFileExtensions: [
+        'json',
+    ],
     module: {
         rules: [
             {
